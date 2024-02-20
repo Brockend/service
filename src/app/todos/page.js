378d59bf6a36d7d47fdd2a117b190fb8efb35f56
Brockend <brockendilya@gmail.com>
2024-02-21 01:18:15 +0200

    Fixed fonts

--- src/app/todos/page.js
+++ src/app/todos/page.js
@@ -1,13 +1,19 @@
+import { Montserrat } from 'next/font/google'
+
 import TodoList from "../components/TodoList/TodoList";
 
 import "../globals.scss";
 import "../main-page.scss";
 
+const montserrat = Montserrat({  subsets: [], display: 'swap' })
+
 export default function Todos() {
     return (
-        <main className="main">
-            <h1 className="main__title">Todos</h1>
-            <TodoList/>
+        <main className={montserrat.className} style={{flex: 1}}>
+            <div className='main__wrapper'>
+                <h1 className="main__title">Todos</h1>
+                <TodoList/>
+            </div>
         </main>
     )
 }
